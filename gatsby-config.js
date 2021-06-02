4065238fff48a2988145f815d292ed387f105635
Andreasmalleus <andreasmalleus@gmail.com>
2021-06-02 19:08:16 +0300

    gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "Portfolio site",
+  },
+  plugins: ["gatsby-plugin-sass"],
+};
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const Index = () => {
+  return (
+    <div>Index page</div> 
+  )
+}
+
+export default Index
