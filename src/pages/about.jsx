b17193ad22d55a7526240585cb24da5fcb1bbc0c
Andreas Malleus <andreasmalleus@gmail.com>
2021-06-13 11:08:30 +0300

    usage of static image

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,14 @@
 import * as React from "react"
 import Layout from "../components/layout";
-import AboutImage from "../assets/profile.png";
 import "../styles/about.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
 const About = () => {
 
   return (
     <Layout pageNr={"01"} title="about">
       <div className="about">
-          <img src={AboutImage} alt="A person looking up"/>
+          <StaticImage src={"../assets/profile.png"} alt="A person looking up" className="about-image"/>
           <div>Hey my name is Andreas Malleus and im an aspiring web developer</div>
       </div>
     </Layout>
