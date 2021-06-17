36a9dad8375c708036d0c42ab66b2df08a163448
Andreas Malleus <andreasmalleus@gmail.com>
2021-06-17 11:35:06 +0300

    seo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,43 +1,51 @@
-import * as React from "react"
+import * as React from "react";
 import "../index.scss";
 import { useEffect } from "react";
 import { navigate } from "gatsby";
-import {CSSTransition} from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 import { useState } from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import SEO from "../components/seo";
 
 const Index = () => {
-
-  const [transition, setTransition] = useState(false)
+  const [transition, setTransition] = useState(false);
 
   useEffect(() => {
-    setTransition(true)
+    setTransition(true);
     setTimeout(() => {
-      navigate("/projects")
-    }, 4000)
-  }, [])
+      navigate("/projects");
+    }, 4000);
+  }, []);
 
   return (
-    <CSSTransition
-      in={transition}
-      timeout={1000}
-      classNames="hero-image-transition"
-      appear
-      unmountOnExit
-    >
-      <div className="hero-container">
-        <div className="hero-content">
-          <div>andreas</div>
-          <span> / </span>
-          <div>malleus</div>
-          <div className="hero-spacing"></div>
-          <div>personal portfolio</div>
-        </div>  
-        <StaticImage src={"../assets/hero.png"} alt="" class="hero-image" layout="fullWidth" quality={100}/>
-      </div>      
-    </CSSTransition>
-
-  ) 
-}
+    <>
+      <SEO />
+      <CSSTransition
+        in={transition}
+        timeout={1000}
+        classNames="hero-image-transition"
+        appear
+        unmountOnExit
+      >
+        <div className="hero-container">
+          <div className="hero-content">
+            <div>andreas</div>
+            <span> / </span>
+            <div>malleus</div>
+            <div className="hero-spacing"></div>
+            <div>personal portfolio</div>
+          </div>
+          <StaticImage
+            src={"../assets/hero.png"}
+            alt=""
+            className="hero-image"
+            layout="fullWidth"
+            quality={100}
+          />
+        </div>
+      </CSSTransition>
+    </>
+  );
+};
 
 export default Index;
